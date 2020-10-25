c305e0eb29d4d2c6aa5672b9b2cab39903671f59
Solanya <paul.corlay@gmail.com>
2020-10-25 16:45:27 +0100

    Optional some stuff

--- SolanyaTweaks.lua
+++ SolanyaTweaks.lua
@@ -1,24 +1,41 @@
 SolanyaTweaksMixin = {}
 
+local guildCountButtonEnabled = true;
+local hideHelmetButtonEnabled = true;
+local randomStuffEnabled = true;
+
 local function PersonalTweaks()
 	PlayerPowerBarAlt:SetPoint("BOTTOM", 0, 220);
 	CastingBarFrame:SetPoint("BOTTOM", 0, 220);
 end
 
 function SolanyaTweaksMixin:OnLoad()
-    self:RegisterEvent("GUILD_ROSTER_UPDATE");
-    self:RegisterEvent("PLAYER_GUILD_UPDATE");
-    self:RegisterEvent("ADDON_LOADED");	-- Set collection checkbox
-	self:RegisterEvent("PLAYER_ENTERING_WORLD");	-- Casting bar
-    self:UpdateDisplayedGuildCount();
-    self.button:SetDesaturated(1);
-    self.button:SetVertexColor(0.3, 1, 0.3);
+	-- Guild count button
+	if guildCountButtonEnabled then
+		self:RegisterEvent("GUILD_ROSTER_UPDATE");
+		self:RegisterEvent("PLAYER_GUILD_UPDATE");
+		self:UpdateDisplayedGuildCount();
+		self.button:SetDesaturated(1);
+		self.button:SetVertexColor(0.3, 1, 0.3);
+	else
+		self:Hide();
+	end
+	-- Hide helmet in set collection
+	if hideHelmetButtonEnabled then
+		self:RegisterEvent("ADDON_LOADED");
+	end
+	-- Casting bar
+	if randomStuffEnabled then
+		self:RegisterEvent("PLAYER_ENTERING_WORLD");
+	end
 end
 
 function SolanyaTweaksMixin:OnEvent(event, ...)
-	if event == "PLAYER_ENTERING_WORLD" then	-- Casting bar
+	if event == "PLAYER_ENTERING_WORLD" then
+		-- Casting bar
 		C_Timer.After(2, PersonalTweaks)
-	elseif event == "ADDON_LOADED" then	-- Set collection checkbox
+	elseif event == "ADDON_LOADED" then
+		-- Hide helmet in set collection
 		if ... == "Blizzard_Collections" then
 			local hideHelmetButton = CreateFrame("Button", "SolanyaTweaksHideHelmetButton", WardrobeCollectionFrame.SetsCollectionFrame.DetailsFrame);
 			hideHelmetButton:SetPoint("TOPLEFT", 10, -10);
@@ -28,6 +45,7 @@ function SolanyaTweaksMixin:OnEvent(event, ...)
 			hideHelmetButton:SetScript("OnClick", function() WardrobeCollectionFrame.SetsCollectionFrame.Model:UndressSlot(1) end)
 		end
 	else
+		-- Guild count button
 		self:UpdateDisplayedGuildCount();
 	end
 end
@@ -40,7 +58,7 @@ function SolanyaTweaksMixin:UpdateDisplayedGuildCount()
     self.count:SetText(onlineMembers - 1);  -- Minus 1 to exclude myself
 end
 
-function SolanyaTweaksMixin:OnClick(button)
+function SolanyaTweaksMixin:OnClick()
     ToggleGuildFrame();
 end
 
